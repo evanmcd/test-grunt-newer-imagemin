4680e602478689ecccdbfcbb732148f24d13f731
Evan McDaniel <emcdaniel@agencypja.com>
2013-11-26 15:28:37 -0500

    Added copy module and task

--- gruntfile.js
+++ gruntfile.js
@@ -5,9 +5,17 @@
  */
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
 
   grunt.initConfig({
+    copy: {
+      target: {
+        files: [
+        {expand: true, cwd: 'Source/', src: ['**','.htaccess','!site/assets/sessions/sess_*','!site/assets/logs/*','!site/assets/cache/*.cache'], dest: 'Runtime/'}
+        ]
+      }
+    },
     imagemin: {
       main: {
         files: [{
@@ -20,6 +28,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('test', ['newer:imagemin:main']);
+  grunt.registerTask('test', ['copy','newer:imagemin:main']);
   grunt.registerTask('default', 'test');
 };
